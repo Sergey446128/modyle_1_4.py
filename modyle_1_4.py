878e6c604ec52ca698c10df3c2051f8a0b18d0ae
Sergey446128 <s.milyaev2018@yandex.ru>
2024-08-27 14:42:09 +0300

    Add files via upload

---
+++ modyle_1_4.py
@@ -0,0 +1,9 @@
+# my_string = input('Здравствуйте:')
+# print(len(my_string))
+
+print('my_string'. upper())
+print('my_string')
+print('my_string'.replace(' ',''))
+name ='my_string'
+print(name[0])
+print(name[- 1])
